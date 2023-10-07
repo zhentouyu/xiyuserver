3a85c57eabe5f91df94af5e513e3d356062d57f2
针头鱼 <111236766+zhentouyu@users.noreply.github.com>
2023-10-07 22:55:34 +0800

    留言显示ip
    添加 留言显示ip(因为某些sb乱搞)

--- issue/index.php
+++ issue/index.php
@@ -7,6 +7,16 @@
         <link rel="stylesheet" href="/css/dropdown.css" />
         <script src="https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
         <script>
+
+            var re = /:/;
+            reipv6 = re.test("<?php echo $_SERVER['REMOTE_ADDR']; ?>");
+            if (reipv6 == false) {
+                var ip = "<?php require "../ip.php"; ?>";
+            }
+            if (reipv6 == true) {
+                var ip = "<?php echo $_SERVER['REMOTE_ADDR']; ?>";
+            }
+
             function postsend() {
                 var url = 'https://wxpusher.zjiecode.com/api/send/message';
                 var text = $("#text").val();
@@ -19,7 +29,7 @@
                 }
                 var data = {
                     "appToken":"AT_55nRtejsJvKRI6YDyt9dXA47aI9fWNz4",
-                    "content":text+"\n"+name,
+                    "content":text+"\n"+name+"\n"+ip,
                     "summary":"新的留言",
                     "contentType":1,
                     "uids":[
@@ -85,5 +95,17 @@
         <small>有急事还是要及时联系细鱼哦<br>有添加什么功能想法之类的可以在这里说一说 如果你会代码的也可以来找我合作啦</small>
         <br>
         <small>有精力的可以去<a href="https://github.com/zhentouyu/xiyuserver/pulls" target="_blank" title="也许要梯子 国内访问过于不稳定">交个PR</a> 虽然我也看不明白那个怎么用 没人交过所以我也不会搞（</small>
+        <br>
+        <small>你的ip
+        <?php
+            if (preg_match("/:/",$_SERVER['REMOTE_ADDR']) === 1) {
+                $ip = $_SERVER['REMOTE_ADDR'];
+                echo $ip;
+            }elseif (preg_match("/:/",$_SERVER['REMOTE_ADDR']) === 0) {
+                require "../ip.php";
+                $ip = $contents;
+            }//正则表达匹配ip是v4/v6 有:表示v6无需转v4 else没有需要转v4获取
+
+        ?></small>
     </body>
 </html>
--- issue/send.php
+++ issue/send.php
@@ -1,14 +1,25 @@
 <?php
 $name = $_POST["name"];
 $text = $_POST["text"];
+
+if (preg_match("/:/",$_SERVER['REMOTE_ADDR']) === 1) {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    echo $ip;
+}elseif (preg_match("/:/",$_SERVER['REMOTE_ADDR']) === 0) {
+    require "../ip.php";
+    $ip = $contents;
+}//正则表达匹配ip是v4/v6 有:表示v6无需转v4 else没有需要转v4获取
+
+
 if (empty($name)) {
     $name = "匿名";
 }
+
 echo $name," ",$text;
 // 创建连接
 $conn = require "../login/database.php";
  
-$sql = "INSERT INTO issue (`name`,`text`) VALUES ('".$name."','".$text."')";
+$sql = "INSERT INTO issue (`name`,`text`,`ip`) VALUES ('".$name."','".$text."','".$ip."')";
 $result = mysqli_query($conn, $sql);
 
 
