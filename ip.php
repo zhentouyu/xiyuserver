96b80867397b03eeb56c663734cfc7eb613907e3
针头鱼 <111236766+zhentouyu@users.noreply.github.com>
2023-11-19 13:20:07 +0800

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,5 +1,6 @@
 <?php
-$url = "43.134.12.4:85/ip.php";
+$url = "101.200.46.93:85/ip.php";
+//$url = "43.134.12.4:85/ip.php";//腾讯云的 不用了
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
